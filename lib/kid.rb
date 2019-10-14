b665913ecbe2a4b1c94d0dc41ed7de32368ac4a0
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-10-14 06:35:29 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,19 +1,24 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
-class Kid #defining the Classes
- extend MetaDancing
+# class Kid #defining the Classes
+# extend MetaDancing
  
- # To lend our two classes all of the methods of 
- # the Dance module, we use the include keyword:
+# # To lend our two classes all of the methods of 
+# # the Dance module, we use the include keyword:
 
-  include Dance
+#   include Dance
  
-  attr_accessor :name
+#   attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
+#   def initialize(name)
+#     @name = name
+#   end
   
   
+# end
+
+class Kid
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 end
